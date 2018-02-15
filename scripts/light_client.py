aa75bd1ff4bd3b8fcd086a53674b914c35d5f9a1
David Dawnkaski <dawnkd@rpi.edu>
2018-02-15 18:54:38 -0500

    added any missing files from last commit

---
+++ scripts/light_client.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+#The light client allows the user to change the color of one light once
+#or to change the color of one light between two colors a number of times
+#The LightState and TestLightChange services  handle communication between
+#the client and the server.
+
+#To change light color once:
+#rosrun light light_client.py address color
+
+#To change between 2 colors x times:
+#rosrun light light_client.py address color1 color2 x
+
+#address is last 3 numbers of IP (111,112,...) in both cases
+
+import sys
+import rospy
+import socket
+from light.srv import *
+from light.msg import *
+
+
+
+def sample_light_client(address,state):
+    rospy.wait_for_service('light_state')
+    try:
+        light_state = rospy.ServiceProxy('light_state',LightState)
+        newState = light_state(address,state)
+        return newState
+    except rospy.ServiceException, e:
+        print("Service call failed: %s"%e)
+
+def test_light_change_client(adress,state1,state2,count):
+    rospy.wait_for_service('test_light_change')
+    try:
+        test_light_change = rospy.ServiceProxy('test_light_change',TestLightChange)
+        finalState = test_light_change(adress,state1,state2,count)
+        return finalState
+    except rospy.ServiceException, e:
+        print("Service call failed: %s"%e)
+
+
+def usage():
+    return "%s light_name new_state\n%s light_name state1 state2 count" % (sys.argv[0],sys.argv[0])
+
+if(__name__ == "__main__"):
+    if (len(sys.argv) == 3):
+        name = str(sys.argv[1])
+        state = str(sys.argv[2])
+
+        address = "192.168.0."+name
+
+        print("Changing light %s to %s"%(address,state))
+
+        new_state = sample_light_client(address,state)
+
+        print("Light %s is now in state %s"%(address,new_state))
+
+    elif (len(sys.argv) == 5):
+        name = str(sys.argv[1])
+        state1 = str(sys.argv[2])
+        state2 = str(sys.argv[3])
+        count = int(sys.argv[4])
+
+        address = "192.168.0."+name
+
+        print("Changing light %s between %s and %s %s times"%(address,state1,state2,count))
+
+        final_state = test_light_change_client(address,state1,state2,count)
+
+        print("Light %s changed between %s and %s %s times and is now in state %s"%(address,state1,state2,count,final_state))
+
+    else:
+        print usage()
+        sys.exit(1)
+
+
+    #pub = rospy.Publisher('sample_light_server', lightbulb, queue_size=10)
+
+    #rospy.init_node('light_switch')
+
+    #msg = lightbulb()
+    #msg.name = host
+    #msg.state = sample_light_client(name,state)
+    #pub.publish(msg)
---
+++ scripts/light_server.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+#The light server recieves address, color(s), and an optional
+#count from the client.
+#Using the color(s), command strings are created.
+#Using the address, a socket for a specific light is opended.
+#The command string is sent to the light.
+#If a second color and a count were recieved, the light will
+#change between the two colors 'count' times at a rate
+#specified by the delay variable.
+
+#To run server:
+#rosrun light light_server.py
+
+#In order to change the rate color is changed at, the server
+#must be stopped using ^C and catkin_make must be called in
+#~/catkin_ws before running the server again
+
+#The LightState and the TestLightChange services are used to
+#communicate between the server and the client.
+
+from light.srv import *
+import rospy
+import socket
+import time
+
+def gen_cmdstr(state):
+    #generates command string based on color recieved by client
+    blue = green = amber = red = white  = "0000"
+
+    if (state == "blue"):
+        blue = "FFFF"
+    elif (state == "green"):
+        green = "FFFF"
+    elif (state == "amber"):
+        amber = "FFFF"
+    elif (state == "red"):
+        red = "FFFF"
+    elif (state == "white"):
+        white = "FFFF"
+
+    return "PS"+blue+green+amber+red+white
+
+def handle_light_state(req):
+    port = 57007
+
+    cmdstr = gen_cmdstr(req.color)
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((req.address,port))
+    except ConnectionRefusedError:
+        rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        rs.connect((req.address,57011))
+	print("Connection refused on " + req.address)
+
+    s.send(cmdstr)
+    a = s.recv(1024)
+    time.sleep(1)
+
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+
+    return LightStateResponse(cmdstr)
+
+def handle_test_light_change(req):
+    port = 57007
+    cmdstr1 = gen_cmdstr(req.color1)
+    cmdstr2 = gen_cmdstr(req.color2)
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((req.address,port))
+    except ConnectionRefusedError:
+        rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        rs.connect((req.address,57011))
+	print("Connection refused on " + req.address)
+
+    delay = 0.005
+
+    #sends the two command strings count times at specified rate
+    for i in range(req.count):
+        s.send(cmdstr1)
+        a = s.recv(1024)
+        time.sleep(delay)
+
+        s.send(cmdstr2)
+        a = s.recv(1024)
+        time.sleep(delay)
+
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+
+    return TestLightChangeResponse(cmdstr2)
+
+def light_state_server():
+    rospy.init_node('light_state_server')
+    # new service called light_state with LightState service type, requests passed to handle...
+    state_serv =  rospy.Service('light_state', LightState, handle_light_state)
+    # new service called test_light_change with TestLightChange service type, requests passed to handle...
+    change_serv = rospy.Service('test_light_change', TestLightChange, handle_test_light_change)
+    print("Ready to turn lights on")
+    rospy.spin()
+
+if(__name__ == "__main__"):
+    light_state_server()
