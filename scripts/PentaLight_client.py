94e783a8ef9f7363531dbd810a8fc4f2d2aedfdd
onetarp <dawnkd@rpi.edu>
2018-03-01 13:59:40 -0500

    pentalight server and client done

---
+++ scripts/PentaLight_client.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import socket
+from light.srv import *
+from light.msg import *
+
+def CCT_client(CCT_val,intensity_val,x,y):
+	rospy.wait_for_service('CCT')
+	try:
+		#					service name  .srv file name
+		CCT = rospy.ServiceProxy('CCT',PentaLight_CCT)
+		state = CCT(CCT_val,intensity_val,x,y)
+		return state
+	except rospy.ServiceException, e:
+		print("Service call failed: %s"%e)
+
+def ragbw_client():
+	rospy.wait_for_service('ragbw')
+	try:
+		#					service name  .srv file name
+		CCT = rospy.ServiceProxy('ragbw',PentaLight_ragbw)
+		state = CCT(CCT_val,intensity_val,x,y)
+		return state
+	except rospy.ServiceException, e:
+		print("Service call failed: %s"%e)
+
+def usage():
+	print("%s CCT intensity x_coord y_coord"%sys.argv[0])
+	print("%s red amber green blue white x_coord y_coord"%sys.argv[0])
+	print("%s HELP"%sys.argv[0])
+
+if(__name__ == "__main__"):
+	if (len(sys.argv) == 5):
+		CCT_val = int(sys.argv[1])
+		intensity_val = int(sys.argv[2])
+		x = int(sys.argv[3])
+		y = int(sys.argv[4])
+
+		print("Changing light (%s,%s) to CCT %s and intensity%s"%(x,y,CCT_val,intensity_val))
+
+		state = CCT_client(CCT_val,intensity_val,x,y)
+
+		print("Light (%s,%s) is now in state %s"%(x,y,state))
+
+	elif (len(sys.argv) == 8):
+		red_val = int(sys.argv[1])
+		amber_val = int(sys.argv[2])
+		green_val = int(sys.argv[3])
+		blue_val = int(sys.argv[4])
+		white_val = int(sys.argv[5])
+		x = int(sys.argv[6])
+		y = int(sys.argv[7])
+
+		print("Changing light (%s,%s) to red:%s%% amber:%s%% green:%s%% blue:%s%% white:%s%%"%(x,y,red_val,amber_val,green_val,blue_val,white_val))
+
+		state = ragbw_client(red_val,amber_val,green_val,blue_val,white_val,x,y)
+
+		print("Light (%s,%s) is now in state %s"%(x,y,state))
+
+	elif (len(sys.argv) == 2):
+		arg = str(sys.argv[1])
+		arg = arg.lower()
+
+		if (arg == "help"):
+			print("how to use the function")
+		else:
+		usage()
+
+	else:
+		usage()
--- scripts/SCR_PentaLight_server.py
+++ scripts/SCR_PentaLight_server.py
@@ -7,7 +7,7 @@ import time
 import sys
 
 def initialize():
-	config = open("SCR_PentaLight_conf.txt",'r')
+	config = open('SCR_PentaLight_conf.txt','r')
 	lights = []
 	addresses = []
 	position_array = []
@@ -84,6 +84,7 @@ def initialize_CCT():
 	for i in range(len(CCT_vals)):
 		CCT_dict[CCT_vals[i]] = light_vals[i]
 
+	#ragbw
 	return CCT_dict
 
 def initialize_int():
@@ -92,34 +93,132 @@ def initialize_int():
 	for line in vals:
 		line = line.rstrip()
 		split = line.split("\t")
-		ints.append[line]
+		ints.append(split)
 	vals.close()
 
-	print ints
+	#bgarw
 	return ints
 
+def gen_cmdstr_CCT(state,intensity,CCT_dict,int_list)
+	#blue = green = amber = red = white  = "0000"
 
-def handle_CCT(req,lights,CCT_dict):
-	return
+	send_blue = CCT_dict[state][3]
+	send_green = CCT_dict[state][2]
+	send_amber = CCT_dict[state][1]
+	send_red = CCT_dict[state][0]
+	send_white = CCT_dict[state][4]
 
-def handle_int(req,lights):
-	return
+	intensity = float(intensity)/100
+
+	intens_b = int_list[0][0]*intensity*intensity+int_list[0][1]*intensity
+	intens_g = int_list[1][0]*intensity*intensity+int_list[1][1]*intensity
+	intens_a = int_list[2][0]*intensity*intensity+int_list[2][1]*intensity
+	intens_r = int_list[3][0]*intensity*intensity+int_list[3][1]*intensity
+	intens_w = int_list[4][0]*intensity*intensity+int_list[4][1]*intensity
+
+	blue = str(int(65535*send_blue*intens_b))
+	green = str(int(65535*send_green*intens_g))
+	amber = str(int(65535*send_amber*intens_a))
+	red = str(int(65535*send_red*intens_r))
+	white = str(int(65535*send_white*intens_w))
+
+	return "PS"+blue+green+amber+red+white
+
+def gen_cmdstr_ragbw(req):
+	blue = str(int(65535*(req.blue/100)))
+	green = str(int(65535*(req.green/100)))
+	amber = str(int(65535*(req.amber/100)))
+	red = str(int(65535*(req.red/100)))
+	white = str(int(65535*(req.white/100)))
+
+	return "PS"+blue+green+amber+red+white
+
+def handle_CCT(req,lights,CCT_dict,int_list):
+	port = 57007
+
+	CCT = 1800
+	intensity = 100
+
+	if(req.CCT < 1800):
+		CCT = 1800
+	elif (req.CCT > 10000):
+		CCT = 10000
+	else:
+		CCT = req.CCT - req.CCT%100
+
+	if (req.intensity < 0):
+		intensity = 0
+	elif (req.intensity > 100):
+		intensity = 100
+	else:
+		intensity = req.intensity
+
+	cmdstr = gen_cmdstr_CCT(CCT,intensity,CCT_dict,int_list)
+
+	if not (req.x,req.y) in CCT_dict:
+		return "Error: No light at specified coordinates"
+
+	address = lights[(req.x,req.y)]
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((address,port))
+	except ConnectionRefusedError:
+		rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		rs.connect((address,57011))
+		print("Connection refused on " + req.address)
+
+	s.send(cmdstr)
+	a = s.recv(1024)
+	time.sleep(0.001)
+
+	s.shutdown(socket.SHUT_RDWR)
+	s.close()
+	
+	return PentaLight_CCTResponse(cmdstr)
+
+# def handle_int(req,lights):
+# 	return
 
 def handle_ragbw(req,lights):
-	return
+	port = 57007
 
-def PentaLight_server(lights,CCT_dict):
+	cmdstr = gen_cmdstr_ragbw(req)
+
+	if not (req.x,req.y) in CCT_dict:
+		return "Error: No light at specified coordinates"
+
+	address = lights[(req.x,req.y)]
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((address,port))
+	except ConnectionRefusedError:
+		rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		rs.connect((address,57011))
+		print("Connection refused on " + req.address)
+
+	s.send(cmdstr)
+	a = s.recv(1024)
+	time.sleep(0.001)
+
+	s.shutdown(socket.SHUT_RDWR)
+	s.close()
+
+	return PentaLight_ragbwResponse(cmdstr)
+
+def PentaLight_server(lights,CCT_dict,int_list):
 	rospy.init_node("PentaLight_server")
 
 	CCT_service = rospy.Service(
 		"CCT", 
 		PentaLight_CCT, 
-		lambda msg: handle_CCT(msg,lights,CCT_dict))
+		lambda msg: handle_CCT(msg,lights,CCT_dict,int_list))
 
-	intensity_service = rospy.Service(
-		"intensity", 
-		PentaLight_int, 
-		lambda msg: handle_int(msg,lights))
+	# intensity_service = rospy.Service(
+	# 	"intensity", 
+	# 	PentaLight_int, 
+	# 	lambda msg: handle_int(msg,lights,int_list))
 
 	ragbw_service = rospy.Service(
 		"ragbw", 
@@ -130,6 +229,6 @@ if (__name__ == "__main__"):
 	lights = initialize()
 	CCT_dict = initialize_CCT()
 	int_list = initialize_int()
-	# PentaLight_server(lights,CCT_dict)
+	PentaLight_server(lights,CCT_dict,int_list)
 
 
