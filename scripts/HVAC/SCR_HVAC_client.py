3758763557fa81c79d54358ee5e8521a0eb9a260
Martin Duffy <duffym4@rpi.edu>
2018-06-25 17:02:35 -0400

    Improvements to HVAC client/server

--- scripts/HVAC/SCR_HVAC_client.py
+++ scripts/HVAC/SCR_HVAC_client.py
@@ -1,139 +1,80 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-
-import sys
-import rospy
-import socket
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "utils"))
+import utils
 from scr_control.srv import *
 from scr_control.msg import *
 
-def setTemp_client(temp):
-	rospy.wait_for_service("setTemp")
-	try:
-		setTemp = rospy.ServiceProxy("setTemp",HVAC_SetTemp)
-		response = setTemp(temp)
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def set_temp(temp, debug = False):
+	state = utils.service_call('set_temp', HVAC_SetTemp, [temp])
+	if state and debug:
+		print(state)
+	return state
 
-def setFanSp_client(speed):
-	rospy.wait_for_service("setTemp")
-	try:
-		setFanSp = rospy.ServiceProxy("setFanSp",HVAC_SetFanSp)
-		response = setFanSp(speed)
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def set_fansp(speed, debug = False):
+	state = utils.service_call('set_fansp', HVAC_SetFanSp, [speed])
+	if state and debug:
+		print(state)
+	return state
 
-def setEp_client(ep,val):
-	rospy.wait_for_service("setTemp")
-	try:
-		setEp = rospy.ServiceProxy("setEp",HVAC_SetEp)
-		response = setEp(ep,val)
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def set_ep(ep, val, debug = False):
+	state = utils.service_call('set_ep', HVAC_SetEp, [ep, val])
+	if state and debug:
+		print(state)
+	return state
 
-def setBms_client():
-	rospy.wait_for_service("setTemp")
-	try:
-		setBms = rospy.ServiceProxy("setBms",HVAC_SetBms)
-		response = setBms()
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def set_bms(debug = False):
+	state = utils.service_call('set_bms', HVAC_SetBms, [])
+	if state and debug:
+		print(state)
+	return state
 
-def getTemp_client():
-	rospy.wait_for_service("setTemp")
-	try:
-		getTemp = rospy.ServiceProxy("getTemp",HVAC_GetTemp)
-		response = getTemp()
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def get_temp(debug = False):
+	state = utils.service_call('get_temp', HVAC_GetTemp, [])
+	if state and debug:
+		print(state)
+	return state
 
-def getEp_client():
-	rospy.wait_for_service("setTemp")
-	try:
-		getEp = rospy.ServiceProxy("getEp",HVAC_GetEp)
-		response = getEp()
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def get_ep(debug = False):
+	state = utils.service_call('get_ep', HVAC_GetEp, [])
+	if state and debug:
+		print(state)
+	return state
 
-def getCO2_client():
-	rospy.wait_for_service("setTemp")
-	try:
-		getCO2 = rospy.ServiceProxy("getCO2",HVAC_GetCO2)
-		response = getCO2()
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def get_co2(debug = False):
+	state = utils.service_call('get_co2', HVAC_GetCO2, [])
+	if state and debug:
+		print(state)
+	return state
 
-def getRH_client():
-	rospy.wait_for_service("setTemp")
-	try:
-		getRH = rospy.ServiceProxy("getRH",HVAC_GetRH)
-		response = getRH()
-		return response
-	except rospy.ServiceException, e:
-		print("Service call failed: %s"%e)
+def get_rh(debug = False):
+	state = utils.service_call('get_rh', HVAC_GetRH, [])
+	if state and debug:
+		print(state)
+	return state
 
-def usage():
-	print("usage:")
-	print("%s set_Temp [Temp]"%sys.argv[0])
-	print("%s set_FanSp [FanSp]"%sys.argv[0])
-	print("%s set_Ep [ep] [value]"%sys.argv[0])
-	print("%s set_Bms"%sys.argv[0])
-	print("%s get_Temp"%sys.argv[0])
-	print("%s get_Ep"%sys.argv[0])
-	print("%s get_CO2"%sys.argv[0])
-	print("%s get_RH"%sys.argv[0])
-	print("%s help"%sys.argv[0])
+# Show help regarding blind commands
+def help(debug=False):
+	helpFile = open(os.path.join(os.path.dirname(__file__), 'SCR_HVAC_help.txt'))
+	helpStr = helpFile.read()
+	print(helpStr)
+	helpFile.close()
+	return helpStr
 
-if __name__ == '__main__':
-	arg1 = str(sys.argv[1]).lower()
-	if (arg1 == "set_temp" and len(sys.argv) == 3):
-		setTemp_client(float(sys.argv[2]))
-	elif (arg1 == "set_fansp" and len(sys.argv) == 3):
-		setFanSp_client(str(sys.argv[2]))
-	elif (arg1 == "set_ep" and len(sys.argv) == 4):
-		setEp_client(int(sys.argv[2]),int(sys.argv[3]))
-	elif (arg1 == "set_bms" and len(sys.argv) == 2):
-		setBms_client()
-	elif (arg1 == "get_temp" and len(sys.argv) == 2):
-		temp_data = getTemp_client()
-		print(temp_data)
-	elif (arg1 == "get_ep" and len(sys.argv) == 2):
-		ep_data = getEp_client()
-		print(ep_data)
-	elif (arg1 == "get_co2" and len(sys.argv) == 2):
-		co2_data = getCO2_client()
-		print(co2_data)
-	elif (arg1 == "get_rh" and len(sys.argv) == 2):
-		rh_data = getRH_client()
-		print(rh_data)
-	elif (arg1 == "help" and len(sys.argv) == 2):
-		print()
-		print("set_Temp: sets the temperature of the system in degrees celsius")
-		print("          first argument is a temperature in celsius as a float")
-		print()
-		print("set_FanSp: sets the fanspeed for the system")
-		print("           first argument is the fan speed, string = off|low|medium|high")
-		print()
-		print("set_Ep: sets EP for the system")
-		print("        first argument is ep value to set")
-		print("        second argument is the value to set to, integer from 0-100")
-		print()
-		print("set_Bms: enables building maintenance system to take control")
-		print()
-		print("get_Temp: gets current temperature reading from each sensor")
-		print()
-		print("get_CO2: gets current CO2 reading in the room")
-		print()
-		print("get_RH: gets the current relative humidity in the room")
-	else:
-		usage()
+if(__name__ == "__main__"):
 
+					#command     #function    #argument types   #help
+	serviceCalls = {
+					'set_temp':  [set_temp,   [float],          "set_temp [temp]"],
+					'set_fansp': [set_fansp,  [str],            "set_fansp [speed = off|low|medium|high]"],
+					'set_ep':    [set_ep,     [int, int],       "set_ep [ep] [value]"],
+					'set_bms':   [set_bms,    [],               "set_bms"],
+					'get_temp':  [get_temp,   [],               "get_temp"],
+					'get_ep':    [get_ep,     [],               "get_ep"],
+					'get_co2':   [get_co2,    [],               "get_co2"],
+					'get_rh':    [get_rh,     [],               "get_rh"],
+					'help':      [help,       [],               "help"]}
 
+	state = utils.commandToFunction(sys.argv, serviceCalls, debug=True) 
