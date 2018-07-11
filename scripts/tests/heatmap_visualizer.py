6426e2a33f31fb7a044d7abf0de1b626e8913005
Martin Duffy <duffym4@rpi.edu>
2018-07-11 13:40:42 -0400

    Improved TOF testing

--- scripts/tests/heatmap_visualizer.py
+++ scripts/tests/heatmap_visualizer.py
@@ -3,9 +3,10 @@ sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "time_of_flight"))
 import SCR_TOF_client as tof
 import matplotlib.pyplot as plt
 
+tof.start_counting()
+
 while True:
-	state = tof.getHeatmap_client()
-	heatmap = numpy.asarray(state.distances)
-	heatmap = numpy.reshape(heatmap, (len(state.distances)/state.room_length, state.room_length))
+	state = tof.get_distances_all()
+	heatmap = numpy.asarray(state)
 	plt.imshow(heatmap)
-	plt.pause(0.05)
+	plt.pause(0.2)
