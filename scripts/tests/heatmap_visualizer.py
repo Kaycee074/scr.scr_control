793ea1d841fed9ce05bbd9f7f04a52104a3ce1b8
Martin Duffy <duffym4@rpi.edu>
2018-07-16 15:14:52 -0400

    Updated config and tests

--- scripts/tests/heatmap_visualizer.py
+++ scripts/tests/heatmap_visualizer.py
@@ -1,12 +1,15 @@
 import sys, os, rospy, numpy, time
-sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "time_of_flight"))
-import SCR_TOF_client as tof
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "lights"))
+import SCR_OctaLight_client as tof
 import matplotlib.pyplot as plt
 
-tof.start_counting()
-
 while True:
-	state = tof.get_distances_all()
+	state = tof.cct_all(1000, 1000)
+	state = tof.cct_all(2000, 1000)
+	'''
 	heatmap = numpy.asarray(state)
+	#time.sleep(.1)
 	plt.imshow(heatmap)
-	plt.pause(0.2)
+	plt.pause(.001)
+	'''
+	print(state)
