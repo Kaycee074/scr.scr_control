c41ef55416e392f294f4ba20e4d73022f287e52c
Martin Duffy <duffym4@rpi.edu>
2018-07-18 11:31:39 -0400

    Reverted TOF back to TCP

--- scripts/time_of_flight/SCR_TOF_client.py
+++ scripts/time_of_flight/SCR_TOF_client.py
@@ -1,23 +1,55 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import numpy
-sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "utils"))
+import sys, os, numpy
+sys.path.append(os.path.join(os.path.dirname(__file__), "../utils"))
 import utils
 from scr_control.srv import *
 from scr_control.msg import *
 
+
+def converter(distances, row_length):
+	heatmap = numpy.asarray(distances)
+	heatmap = numpy.reshape(heatmap, (len(distances)/row_length, row_length))
+
+	return heatmap	
+
+
 # Get a heatmap of the room (returns int16[] distances and int16 room_length)
-def get_distances(debug=False):
-	state = utils.service_call('get_heatmap', TOFGetDistancesAll, [])
+def get_distances(sensor_id, debug=False):
+	state = utils.service_call('get_distances', TOFGetDistances, [sensor_id])
+	if debug:
+		print(state)
 	if state:
-		dist = numpy.asarray(state.distances)
-		dist = numpy.reshape(heatmap, (len(state.distances)/160, 160))
-		dist = dist.tolist()
-		if debug:
-			print(dist)
-		return dist
+		return converter(state.data)
+
+	return None
+
+# Get a heatmap of the room (returns int16[] distances and int16 room_length)
+def get_distances_all(debug=False):
+	state = utils.service_call('get_distances_all', TOFGetDistancesAll, [])
+	if debug:
+		print(state)
+	if state:
+		return converter(state.data, 160)
+
+	return None
+
+
+# Start updating TOF sensors
+def start_counting(debug=False):
+	state = utils.service_call('set_counting', TOFSetCounting, [True])
+	if debug:
+		print("Starting to update TOF sensors...")
+	if state:
+		return state
+
+# Start updating TOF sensors
+def stop_counting(debug=False):
+	state = utils.service_call('set_counting', TOFSetCounting, [False])
+	if debug:
+		print("Stopping TOF sensors...")
+	if state:
+		return state
 
 # Show help regarding TOF commands
 def help(debug=False):
@@ -25,9 +57,12 @@ def help(debug=False):
 
 if(__name__ == "__main__"):
 
-					#command         #function       #argument types   #help
+					#command              #function        	   #argument types   #help
 	serviceCalls = {
-					'get_distances': [get_distances, [],               "get_distances"],
-					'help':          [help,          [],               "help"]}
+					'get_distances_all':  [get_distances_all,  [],               "get_distances_all"],
+					'get_distances':      [get_distances,      [int],            "get_distances [sensor_id]"],
+					'start_counting':     [start_counting,     [],               "start_counting"],
+					'stop_counting':      [stop_counting,      [],               "stop_counting"],
+					'help':               [help,               [],               "help"]}
 
 	state = utils.commandToFunction(sys.argv, serviceCalls, debug=True) 
--- scripts/time_of_flight/SCR_TOF_server.py
+++ scripts/time_of_flight/SCR_TOF_server.py
@@ -15,8 +15,29 @@ class TimeOfFlightServer():
 
 	def __init__(self):
 		print("Starting TOF server")
-		self.data_location = self.read_config()
+		self.address = self.read_config()
 		self.server_init()
+	
+	def read_config(self):
+
+		config = open(os.path.join(os.path.dirname(__file__), 'TOF_conf.txt'))
+		line = config.readline()
+		line = line.rstrip()
+		config.close()
+
+		return line
+
+	def establish_connection(self):
+		port = 3660
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((self.address,port))
+		except:
+			#rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			#rs.connect((self.address, 57011))
+			print("Connection refused on " + self.address)
+		return s
+
 	
 	'''
 	COMMAND HANDLERS
@@ -26,31 +47,89 @@ class TimeOfFlightServer():
 		config = open(os.path.join(os.path.dirname(__file__), 'SCR_TOF_conf.txt'), 'r')
 		return config.readline()
 
-	def handle_get_distances(self, req):
-		distances = []
-		room_length = 0
-		data_file = open(self.data_location, 'r')
+	def handle_getDistances(self, req):
+		data = self.get_sensorData(req.sensor_id)
 
-		for line in data_file:
-			lineData = line.split()
-			if room_length == 0:
-				room_length = len(lineData)
-			for num in lineData:
-				distances.append(int(num))
+		return TOFGetDistancesResponse(data)
 
-		return TOFGetDistancesResponse(distances)
+	#160 x 75
+	def handle_getDistancesAll(self, req):
+		distances =  [0]*(160*75)
+		for i in range(18):
+			startY = 25
+			startX = 0
+			
+			if (i < 8):
+				startY = 50
+				startX = i*20
+			elif (i == 8):
+				startX = 140
+				startY = 25
+			elif (i < 17):
+				startY = 0
+				startX = 140 - (i-9)*20				
 		
+			data = self.get_sensorData(i)
+
+			for j in range(len(data)):
+				x = (j % 20) + startX
+				y = (j / 20) + startY
+				distances[x+y*160] = data[j]
+
+		return TOFGetDistancesAllResponse(distances)
+
+
+	def handle_setCounting(self, req):
+		if req.mode:
+			s = self.establish_connection()
+			s.send("start_counting")
+			#data = s.recv(3000)
+			s.shutdown(socket.SHUT_RDWR)
+			s.close()
+
+		else:
+			s = self.establish_connection()
+			s.send("stop_counting")
+			#data = s.recv(3000)s
+			s.shutdown(socket.SHUT_RDWR)
+			s.close()
+		return TOFSetCountingResponse()
+
+
 	'''
 	HELPER FUNCTIONS
 	'''
 
+	def get_sensorData(self, sensor_id):
+		s = self.establish_connection()
+		s.send("get_data"+str(sensor_id))
+		data = s.recv(3000)
+		s.shutdown(socket.SHUT_RDWR)
+		s.close()
+
+
+		data = data.split(",")
+		data = [int(float(x)) for x in data]
+
+		return data
+
 	def server_init(self):
 		rospy.init_node("time_of_flight_server")
 
-		lift_service = rospy.Service(
+		get_distances_service = rospy.Service(
 			"get_distances",
+			TOFGetDistances,
+			self.handle_getDistances)
+
+		get_distances_all_service = rospy.Service(
+			"get_distances_all",
 			TOFGetDistancesAll,
-			self.handle_get_distances)
+			self.handle_getDistancesAll)
+
+		set_counting_service = rospy.Service(
+			"set_counting",
+			TOFSetCounting,
+			self.handle_setCounting)
 
 		rospy.spin()
 
