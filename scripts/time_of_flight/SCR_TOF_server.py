7478df6aabbb58c88b5854db02fcbce52408dfcb
Martin Duffy <duffym4@rpi.edu>
2019-07-25 14:02:02 -0400

    Fixed TOF reading errors

--- scripts/time_of_flight/SCR_TOF_server.py
+++ scripts/time_of_flight/SCR_TOF_server.py
@@ -6,6 +6,7 @@ import socket
 import time
 import sys
 import os
+import numpy as np
 
 class TimeOfFlightServer():
 
@@ -17,7 +18,9 @@ class TimeOfFlightServer():
 		print("Starting TOF server")
 		self.data_location = '/home/arunas/tof_control/SCR/output-'
 		self.server_init()
-
+        self.data = []
+        for i in range(18):
+            self.data.append(np.zeros((20, 25)))
 	'''
 	COMMAND HANDLERS
 	'''
@@ -26,13 +29,29 @@ class TimeOfFlightServer():
 		config = open(os.path.join(os.path.dirname(__file__), 'SCR_TOF_conf.txt'), 'r')
 		return config.readline()
 
+    def read_sensor(self, i):
+        distances = np.zeros((20, 25))
+        try:
+    		data_file = open(self.data_location + str(i) + ".txt", 'r')
+    		y = 0
+    		for line in data_file:
+    			lineData = line.split()
+    			x = 19
+    			for num in lineData:
+   					distances[ y * 20 + x ] = int(num)
+    				x -= 1
+    			y += 1
+            self.data[i] = distances
+    	    return distances 							
+        except:
+            return self.data[i]
+
 	def handle_get_distances_all(self, req):
-		distances = [0]*160*75
+		distances = np.zeros((160, 75))
 
 		for i in range(18):
 			startY = 25
 			startX = 0
-
 			if (i < 8):
 				startY = 50
 				startX = i*20
@@ -42,38 +61,14 @@ class TimeOfFlightServer():
 			elif (i < 17):
 				startY = 0
 				startX = 140 - (i-9)*20
+            sensor_data = self.read_sensor(i)
+            distances[startX:startX+20, startY:startY+25] = sensor_data
 
-			data_file = open(self.data_location + str(i) + ".txt", 'r')
-			y = startY
-			for line in data_file:
-				lineData = line.split()
-				x = startX + 19
-				for num in lineData:
-					try:
-						distances[ y * 160 + x ] = int(num)
-					#	distances[y * 160 + x] = (int(num, 32))
-					#	distances[y][x] = (int(num,16))
-					except:
-						print("x =", x, "\ny = ", y)
-					x -= 1
-				y += 1
 		return TOFGetDistancesAllResponse(distances)
 
 
 	def handle_get_distances(self, req):
-		distances = [0]*20*25
-		data_file = open(self.data_location + str(req.sensor_id) + ".txt", 'r')
-		y = 0
-		for line in data_file:
-			lineData = line.split()
-			x = 19
-			for num in lineData:
-				try:
-					distances[ y * 20 + x ] = int(num)
-				except:
-					print("x =", x, "\ny = ", y)
-				x -= 1
-			y += 1
+		distances = self.read_sensor(req.sensor_id)
 		return TOFGetDistancesResponse(distances)
 
 	'''
